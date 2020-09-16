b8bcd68b22c7f0ba0881d6b386e22094280e5864
Rubén Adarme <rubenadarme@gmail.com>
2020-09-16 21:15:54 +0200

    #4 modal fixed

--- src/modal.js
+++ src/modal.js
@@ -10,19 +10,19 @@ const Modal = ({person, show, closeModal}) => {
    }
     return(
       <div className="modal-box">
-         <h1>{person.name}</h1>
          <div className="modalcontainer">
+         <h1>{person.name}</h1>
             <p className="modalprops">age: {person.age}</p>
             <p className="modalprops">email: {person.email}</p>
             <p className="modalprops">hometown: {person.hometown}</p>
             <p className="modalprops">hobbies and interests: {person.hobbies}</p>
             <p className="modalprops">buddy or patient? {person.buddy_patient}</p>
-         </div>
-         <div className="btn-container">
+            <div className="btn-container">
             <button className="small-button delete"> Delete </button>
             <button className="small-button close" onClick={closeModal}> Close </button>
         </div>
-        <div className="overlay"></div>
+         </div>
+        <div className="overlay" onClick={closeModal}></div>
       </div>
     )
   };
