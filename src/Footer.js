7a2de2c464bed9c36eb1379ed2a9eeebb509b6eb
Rubén Adarme <rubenadarme@gmail.com>
2020-09-20 17:13:43 +0200

    #9  created the footer component

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+        <div className="footer">
+
+            <a href="https://www.uu.nl/"> 
+                <img src="./images/universidad1.png"
+                alt="Utrecht university logo"
+                className="universidad"/>
+            </a>            
+            <a href="https://migracode.openculturalcenter.org/">
+                <img src="./images/migracode.png"
+                alt="Migracode Barcelona logo"
+                className="migracode"/>
+            </a>
+                
+        </div>  
+}
+
+export default Footer;
