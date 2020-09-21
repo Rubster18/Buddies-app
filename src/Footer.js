04a6faf16585767e6c5a52a513d91df5da498c86
Rubén Adarme <rubenadarme@gmail.com>
2020-09-21 17:42:46 +0200

    #9 homepage half done, uoloading images and the 404 error page this one needs styles, will do them later

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import migracode from './Images/migracode.png'
+import universidad1 from './Images/universidad1.png'
 
 const Footer = () => {
+    return(
         <div className="footer">
 
             <a href="https://www.uu.nl/"> 
-                <img src="./images/universidad1.png"
+                <img src={universidad1}
                 alt="Utrecht university logo"
                 className="universidad"/>
             </a>            
             <a href="https://migracode.openculturalcenter.org/">
-                <img src="./images/migracode.png"
+                <img src={migracode}
                 alt="Migracode Barcelona logo"
                 className="migracode"/>
             </a>
                 
         </div>  
+    )
 }
 
 export default Footer;
