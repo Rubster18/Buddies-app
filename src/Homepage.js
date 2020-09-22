17f7ef361d897299f37546b8fc973daff7760167
Rubén Adarme <rubenadarme@gmail.com>
2020-09-22 18:49:56 +0200

    most styles done, homepage needs a little tweak

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,16 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Button from "./Button";
 import Footer from "./Footer";
+import Viejita from "./Images/viejita.png"
 
 
 const Homepage = () => {
     return (
         <div className="homepage">
             <Header />
-                <div className="jumbotron container">
-                    <h1>Welkom op de beelbuddy</h1> 
-                    <h2> Leuk dat je interesse hebt in het worden van een buddy!</h2>
-
+                <div className="jumbotroncontainer">
+                    <h1 className="white-text">Welkom op de beelbuddy</h1> 
+                    <h2 className="white-text"> Leuk dat je interesse hebt in het worden van een buddy!</h2>
+                    <img src={Viejita} className="jumbotron-image" alt="someone sharing with a patient with a smartphone"/>
                     <div className="homepage-button-container">
                         <Button route="/BuddiesForm" buttonType="big" text="Ik ben een maatje" />
                         <Button route="/StudentForm" buttonType="big" text="Ik wil een maatje" />
@@ -20,12 +21,12 @@ const Homepage = () => {
                 <div className="aboutus">
                     <h2 className="title"> Beelbuddy </h2>
                     <p> Deze site is ontstaan vanuit de gevolgen die het coronavirus heeft op studenten en mensen met een hulpvraag. 
-                        Studenten Social work konden door het coronavirus niet meer op bezoek bij hun maatjes en de maatjes mochten geen studenten meer ontvangen en konden vaak ook niet naar hun werk en dagbesteding, een erg vervelende tijd. 
+                        Studenten Social work konden door het coronavirus niet meer op bezoek bij hun maatjes en de maatjes mochten geen studenten meer ontvangen en konden vaak ook niet naar hun werk en dagbesteding, een erg vervelende tijd.</p> 
 
-                        Dankzij beeldbellen konden beide groepen toch contact met elkaar houden!
-                        En met succes. In april 2020 zijn er 15 studenten social work, vanuit de Hogeschool Utrecht,  gekoppeld aan mensen die behoefte hadden aan een praatje. Dit bleek een groot succes, zowel de studenten als hun maatjes ervaarde dit contact als erg prettig. Sommige hebben nog steeds contact en inmiddels ook in het echt afgesproken. 
+                        <p> Dankzij beeldbellen konden beide groepen toch contact met elkaar houden!
+                        En met succes. In april 2020 zijn er 15 studenten social work, vanuit de Hogeschool Utrecht,  gekoppeld aan mensen die behoefte hadden aan een praatje. Dit bleek een groot succes, zowel de studenten als hun maatjes ervaarde dit contact als erg prettig. Sommige hebben nog steeds contact en inmiddels ook in het echt afgesproken. </p>
 
-                        Heb jij nou ook interesse om iemand zijn beeldbuddy te worden en 1-2 keer per week via een telefoon of tablet of computer contact met elkaar te hebben?
+                        <p> Heb jij nou ook interesse om iemand zijn beeldbuddy te worden en 1-2 keer per week via een telefoon of tablet of computer contact met elkaar te hebben?
                         Vul dan snel het formulier in op de volgende pagina! En wie weet heb jij binnenkort een leuke buddy! 
                     </p>
                 </div> 
--- src/ThankYou.js
+++ src/ThankYou.js
@@ -7,12 +7,12 @@ const Submit = () => {
             <Header />
             <div className="thank-you-container margin-auto">
                 <div className="success-container">
-                    <h1>Success! </h1>
-                    <h2>Your information was sent correctly</h2>
+                    <h1>Succes! </h1>
+                    <h2>De informatie is verzonden</h2>
                 </div>
                 <div className="contact-you-container">
-                    <p> We will contact you to let you know the next steps, thanks for trusting us</p>
-                    <a href="." className="big-button margin-auto">Go back to the home</a>
+                    <p> Wij nemen contact met je op zodra er een match is</p>
+                    <a href="." className="big-button margin-auto">Ga terug naar de homepagina</a>
                 </div>
             </div>
         </div>
